5e5f1616d1249e93a201db5a3538d689bb948534
Experrrt <jakubmarcek955@gmail.com>
2023-02-17 23:14:40 +0100

    init

---
+++ vue/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { onMounted, reactive } from 'vue';
+
+const state = reactive({ count: 0, name: '' });
+
+function increment() {
+  state.count++;
+}
+
+onMounted(() => {
+  state.name = 'halo';
+});
+</script>
+
+<template>
+  <h4>{{ state.name }}</h4>
+  <p>{{ state.count }}</p>
+  <button @click="state.count++">Add</button>
+  <input type="text" v-model="state.name" />
+</template>
